1c174caa972fd0be371d8ae08252aba6bd6e95b5
Yann IRBAH <yann@wengee.co>
2014-02-14 15:48:41 +0100

    Adds a secret.example.rb file for storing credential and other secret configuration

---
+++ config/secret.example.rb
@@ -0,0 +1,6 @@
+# Put all your secret information in this file.
+# (APIs credentials ...)
+#
+# Don't forget to rename this file to secret.rb
+
+# set :secret_api_key, 'imasupersecretapikeynooneshouldsee'
